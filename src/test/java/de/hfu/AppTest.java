76095c11da4eda9507c22ee184ede7a5ea87bba5
Jonas Schlecht <jonas.schlecht16@gmail.com>
2020-11-19 15:34:11 +0100

    Tests formuliert

--- src/test/java/de/hfu/AppTest.java
+++ src/test/java/de/hfu/AppTest.java
@@ -1,6 +1,6 @@
 package de.hfu;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -10,9 +10,55 @@ import org.junit.Test;
 public class AppTest 
 {
     @Test
-    public void testdenqueue() {
+    public void testUtil() {
     	assertTrue(Util.istErstesHalbjahr(1));
+    	assertTrue(Util.istErstesHalbjahr(2));
+    	assertTrue(Util.istErstesHalbjahr(3));
+    	assertTrue(Util.istErstesHalbjahr(4));
+    	assertTrue(Util.istErstesHalbjahr(5));
     	assertTrue(Util.istErstesHalbjahr(6));
-    	assertTrue(Util.istErstesHalbjahr(7));
+    	assertFalse(Util.istErstesHalbjahr(7));
+    	assertFalse(Util.istErstesHalbjahr(8));
+    	assertFalse(Util.istErstesHalbjahr(9));
+    	assertFalse(Util.istErstesHalbjahr(10));
+    	assertFalse(Util.istErstesHalbjahr(11));
+    	assertFalse(Util.istErstesHalbjahr(12));
+    	try {
+    		Util.istErstesHalbjahr(0);
+    		fail();
+    	}
+    	catch(IllegalArgumentException e){
+    	}
+    	try {
+    		Util.istErstesHalbjahr(13);
+    		fail();
+    	}
+    	catch(IllegalArgumentException e){  		
+    	}
+    }
+    @Test
+    public void testQueue() {
+    	Queue tester = new Queue(3);
+    	tester.enqueue(5);
+    	tester.enqueue(6);
+    	tester.enqueue(9);
+    	assertEquals(5, tester.dequeue());
+    	assertEquals(6, tester.dequeue());
+    	assertEquals(9, tester.dequeue());
+    	try {
+    		tester.dequeue();
+    		fail();
+    	}
+    	catch(IllegalStateException e) {
+    		
+    	}
+    	tester.enqueue(5);
+    	assertEquals(5, tester.dequeue());
+    	for (int i = 0; i < 1000; i++ ) {
+    		tester.enqueue(i);
+    	}
+    	assertEquals(0, tester.dequeue());
+    	assertEquals(1, tester.dequeue());
+    	assertEquals(999, tester.dequeue());
     }
 }
